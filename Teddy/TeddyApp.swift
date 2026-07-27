b52b05e345b523e052885cf59a3346daa795385b
livecoderuaridh <ruaridhbell73@googlemail.com>
2026-07-27 19:31:31 +0100

    Initial Commit

---
+++ Teddy/TeddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeddyApp.swift
+//  Teddy
+//
+//  Created by Ruaridh Bell on 27/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TeddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TeddyTests/TeddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeddyTests.swift
+//  TeddyTests
+//
+//  Created by Ruaridh Bell on 27/07/2026.
+//
+
+import Testing
+@testable import Teddy
+
+struct TeddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
